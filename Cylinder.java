3808a18581eeb085c8004b0103472d6253ec93e0
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 14:31:59 +0530

    /

--- Cylinder.java
+++ Cylinder.java
@@ -4,12 +4,15 @@ class Cylinder extends Shape implements Volume {
     public Cylinder(double radius, double height) {
         super(radius, height, 0, 0);
     }
+
     public double calculateArea() {
         return 2 * Math.PI * dim_one * (dim_one + dim_two);
     }
+
     public double calculateVolume() {
         return Math.PI * dim_one * dim_one * dim_two;
     }
+
     public double calculatePerimeter() {
         return 0; // Perimeter is not defined for cylinder
     }
