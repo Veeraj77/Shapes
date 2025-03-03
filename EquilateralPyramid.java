2168eb3ca3d65a912f3e39347176dfd603dacd5e
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 14:53:33 +0530

    /

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -4,14 +4,17 @@ class EquilateralPyramid extends Shape implements Volume {
     public EquilateralPyramid(double base, double height) {
         super(base, height, 0, 0);
     }
+
     public double calculateArea() {
         double baseArea = dim_one * dim_one;
         double sideArea = 4 * (0.5 * dim_one * dim_two);
         return baseArea + sideArea;
     }
+
     public double calculateVolume() {
         return (1.0 / 3.0) * dim_one * dim_one * dim_two;
     }
+
     public double calculatePerimeter() {
         return 4 * dim_one;
     }
