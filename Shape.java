63f4a624aa4707e8b8795f69e0299044ead9c6f5
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 15:57:24 +0530

    creating public shape

--- Shape.java
+++ Shape.java
@@ -3,3 +3,9 @@
 abstract class Shape {
     protected double dim_one, dim_two, dim_three;
     protected int sides;
+    public Shape(double dim_one, double dim_two, double dim_three, int sides) {
+        this.dim_one = dim_one;
+        this.dim_two = dim_two;
+        this.dim_three = dim_three;
+        this.sides = sides;
+    }
