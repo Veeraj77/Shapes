d83e04d1076557cf1e88ad1129f991fc03a22961
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 15:18:53 +0530

    comments

--- Rectangle.java
+++ Rectangle.java
@@ -1,13 +1,19 @@
 //rectangle.java
 
 import java.lang.Math;
+
 class Rectangle extends Shape {
+    
     public Rectangle(double length, double width) {
         super(length, width, 0, 3);  
     }
+
+    // Calculate area of the triangle
     public double calculateArea() {
         return  dim_one * dim_two;  
     }
+
+    // Calculate perimeter
     public double calculatePerimeter() {
         return 2*(dim_one+dim_two);
     }
