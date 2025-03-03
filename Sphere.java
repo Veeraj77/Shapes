5bcf04a6a8987ba6fa39a72b6e49d30611ed223e
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 15:55:25 +0530

    creating calculateVolume

--- Sphere.java
+++ Sphere.java
@@ -7,3 +7,6 @@ class Sphere extends Shape implements Volume {
     public double calculateArea() {
         return 4 * Math.PI * dim_one * dim_one;
     }
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
+    }
