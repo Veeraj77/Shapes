60660078412a799544a2db0876a2c612eee6d6f3
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 14:51:59 +0530

    creating public eqilateralpyramid

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -1,3 +1,6 @@
 // EquilateralPyramid.java
 // Equilateral Pyramid class implementing Volume interface
 class EquilateralPyramid extends Shape implements Volume {
+    public EquilateralPyramid(double base, double height) {
+        super(base, height, 0, 0);
+    }
