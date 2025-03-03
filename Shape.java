f8d8d90a92345e95b985c218ff64050ad38829cc
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 16:00:00 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -3,14 +3,19 @@
 abstract class Shape {
     protected double dim_one, dim_two, dim_three;
     protected int sides;
+
     public Shape(double dim_one, double dim_two, double dim_three, int sides) {
         this.dim_one = dim_one;
         this.dim_two = dim_two;
         this.dim_three = dim_three;
         this.sides = sides;
     }
+
+    // Abstract methods for area and perimeter
     public abstract double calculateArea();
     public abstract double calculatePerimeter();
+}
+
 // Interface for volume calculations
 interface Volume {
     double calculateVolume();
