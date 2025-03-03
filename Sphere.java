d3f181b649e0c99f6acdf00a3842b4b2749154cc
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 15:32:33 +0530

    creating public sphere

--- Sphere.java
+++ Sphere.java
@@ -1,3 +1,6 @@
 // Sphere.java
 // Sphere class implementing Volume interface
 class Sphere extends Shape implements Volume {
+    public Sphere(double radius) {
+        super(radius, 0, 0, 0);
+    }
