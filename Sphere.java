61ca8936cd760c3db155d45d0785b90880b42460
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 15:55:42 +0530

    Update Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -4,12 +4,15 @@ class Sphere extends Shape implements Volume {
     public Sphere(double radius) {
         super(radius, 0, 0, 0);
     }
+
     public double calculateArea() {
         return 4 * Math.PI * dim_one * dim_one;
     }
+
     public double calculateVolume() {
         return (4.0 / 3.0) * Math.PI * Math.pow(dim_one, 3);
     }
+
     public double calculatePerimeter() {
         return 0; // Perimeter is not defined for sphere
     }
