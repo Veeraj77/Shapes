26d7034dd1e3cb9432ee4a43ad6e69bbc304e295
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-03 15:04:22 +0530

    menu option

--- Main1.java
+++ Main1.java
@@ -4,3 +4,15 @@ class Main1 {
         Scanner scan = new Scanner(System.in);
         int choice;
         // Menu driven program loop
+        do {
+            System.out.println("Select Shape:");
+            System.out.println("1. Rectangle");
+            System.out.println("2. Circle");
+            System.out.println("3. Square");
+            System.out.println("4. Sphere");
+            System.out.println("5. Cylinder");
+            System.out.println("6. Equilateral Pyramid");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scan.nextInt();
+            
